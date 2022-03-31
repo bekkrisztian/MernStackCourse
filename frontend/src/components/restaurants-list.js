0ade8f740cf7019aa15447c431f91ce1a4e2225c
Benedek Kirsztián <67368367+bekkrisztian@users.noreply.github.com>
2022-03-31 23:09:10 +0300

    requests to restaurants list

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,6 +1,58 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
 
-function RestaurantsList() {
+const RestaurantsList = (props) => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [searchZip, setSerarchZip] = useState("");
+    const [searchCuisine, setSearchCuisine] = useState("");
+    const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+    useEffect(() => {
+        retriveRestaurants();
+        retriveCuisines();
+    }, []);
+
+    const retriveRestaurants = () => {
+        RestaurantDataService.getAll()
+            .then(response => {
+                console.log(response.data);
+                setRestaurants(response.data.restaurants);
+            }).catch(e => {
+                console.log(e);
+            });
+    };
+
+    const retriveCuisines = () => {
+        RestaurantDataService.getCuisines()
+            .then(response => {
+                console.log(response.data);
+                setCuisines(["All Cuisines"].concat(response.data));
+            }).catch(e => {
+                console.log(e);
+            });
+    };
+
+    const onChangeSearchName = (e) => {
+        const searchName = e.target.value;
+        setSearchName(searchName);
+    };
+
+    const onChangeSearcZip = (e) => {
+        const searchZip = e.target.value;
+        setSerarchZip(searchZip);
+    };
+
+    const onChangeSearchCuisine = (e) => {
+        const searchCuisine = e.target.value;
+        setSearchCuisine(searchCuisine);
+    };
+
+    const refreshList = () => {
+        retriveRestaurants();
+    };
+
     return (
         <div className="App">
             Hello World.
--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -12,4 +12,22 @@ class RestaurantDataService {
     find(query, by = "name", page = 0) {
         return http.get(`?${by}=${query}&page=${page}`);
     }
+
+    createReview(data) {
+        return http.post("/review", data);
+    }
+
+    updateReview(data) {
+        return http.put("/review", data);
+    }
+
+    deleteReview(id) {
+        return http.delete(`/review?id=${id}`);
+    }
+
+    getCuisines(id) {
+        return http.get("/cuisines");
+    }
 }
+
+export default new RestaurantDataService();
